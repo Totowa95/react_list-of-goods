bd5bd0ff91f040005b4bab498b779e85ff032725
Anton <a.v.sorokin95@gmail.com>
2020-09-04 15:36:19 +0300

    Updates

---
+++ precache-manifest.487acaa15094a576f8577a137c4a7f39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19923ba2318abaa630058e359b8fc724",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f802dc6d282c1468bba9",
+    "url": "./static/css/main.b95de14d.chunk.css"
+  },
+  {
+    "revision": "7dca0c430730ddae82af",
+    "url": "./static/js/2.4ff1ba40.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.4ff1ba40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f802dc6d282c1468bba9",
+    "url": "./static/js/main.d9934288.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
